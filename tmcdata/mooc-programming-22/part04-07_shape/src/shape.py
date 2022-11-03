ef2f066eb2b8086b9a9d658a2475e81f016608a0
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 15:48:15 +0100

    Complete exercise: shape

--- tmcdata/mooc-programming-22/part04-07_shape/src/shape.py
+++ tmcdata/mooc-programming-22/part04-07_shape/src/shape.py
@@ -1,4 +1,25 @@
-# Copy here code of line function from previous exercise and use it in your solution
+def line(num, text):
+    if text == "":
+        print("*" * num)
+    else:
+        print(text[0] * num)
+
+
+def shape(num1, text1, num2, text2):
+    counter = 1
+    while counter <= num1:
+        line(counter, text1)
+        counter += 1
+    counter = num2
+    while counter > 0:
+        line(num1, text2)
+        counter -= 1
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    shape(5, "x", 2, "o")
+    shape(5, "X", 3, "*")
+    print()
+    shape(2, "o", 4, "+")
+    print()
+    shape(3, ".", 0, ",")
