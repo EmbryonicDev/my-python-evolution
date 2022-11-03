631e08a29a295e93908eeeb8c263192823d3650b
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 13:12:33 +0100

    Complete exercise: hello visual studio

--- tmcdata/mooc-programming-22/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ tmcdata/mooc-programming-22/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,14 @@
-# Write your solution here
+
+while True:
+    editor = input("Editor: ")
+    editor = editor.lower()
+
+    if editor == "visual studio code":
+        break
+
+    if editor == "emacs" or editor == "vim" or editor == "atom":
+        print("not good")
+    elif editor == "word" or editor == "notepad":
+        print("awful")
+
+print("an excellent choice!")
