1f6acf9fa64d14208e35cae7f0f1bcfce45aaf50
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 17:05:01 +0100

    Complete exercise: greatest number

--- tmcdata/mooc-programming-22/part04-09_greatest_number/src/greatest_number.py
+++ tmcdata/mooc-programming-22/part04-09_greatest_number/src/greatest_number.py
@@ -1,5 +1,13 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def greatest_number(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num3:
+        return num2
+    return num3
+
+
+# test code
 if __name__ == "__main__":
-    greatest = greatest_number(5, 4, 8)
-    print(greatest)
+    print(greatest_number(3, 4, 1))  # 4
+    print(greatest_number(99, -4, 7))  # 99
+    print(greatest_number(0, 0, 0))  # 0
