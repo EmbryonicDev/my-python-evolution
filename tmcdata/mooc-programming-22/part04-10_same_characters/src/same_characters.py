84d141d7510e3af2a564a2fb9c58f9c14314bfd1
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 17:43:56 +0100

    Complete exercise: same characters

--- tmcdata/mooc-programming-22/part04-10_same_characters/src/same_characters.py
+++ tmcdata/mooc-programming-22/part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,15 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def same_chars(text, index1, index2):
+    if index1 < len(text) - 1 and index2 < len(text) - 1:
+        if text[index1] == text[index2]:
+            return True
+    return False
+
+
+# test code
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    # same characters m and m
+    print(same_chars("programmer", 6, 7))  # True
+    # different characters p and r
+    print(same_chars("programmer", 0, 4))  # False
+    # the second index is not within the string
+    print(same_chars("programmer", 0, 12))  # False
