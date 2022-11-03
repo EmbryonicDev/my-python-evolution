4520d6f1fab04071ce932e040fdb6fe36a54ef6b
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 15:24:14 +0100

    Complete exercise: box of hashes

--- tmcdata/mooc-programming-22/part04-03_box_of_hashes/src/box_of_hashes.py
+++ tmcdata/mooc-programming-22/part04-03_box_of_hashes/src/box_of_hashes.py
@@ -1,9 +1,18 @@
-# Copy here code of line function from previous exercise
+def line(num, text):
+    if text == "":
+        print("*" * num)
+    else:
+        print(text[0] * num)
+
 
 def box_of_hashes(height):
-    # You should call function line here with proper parameters
-    line(10, "#")
+    while height > 0:
+        line(10, "#")
+        height -= 1
 
-# You can test your function by calling it within the following block
+
+# test function
 if __name__ == "__main__":
     box_of_hashes(5)
+    print()
+    box_of_hashes(1)
