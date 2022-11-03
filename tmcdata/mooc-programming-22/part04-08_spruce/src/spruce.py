31d5ada95495ce4a0ccb20adbebcc4d9353fbca9
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 16:40:21 +0100

    Complete exercise: spruce

--- tmcdata/mooc-programming-22/part04-08_spruce/src/spruce.py
+++ tmcdata/mooc-programming-22/part04-08_spruce/src/spruce.py
@@ -1,4 +1,15 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def spruce(num):
+    print("a spruce!")
+    space_counter = num - 1
+    star_counter = 1
+    while star_counter <= num * 2:
+        print(" " * space_counter + "*" * star_counter)
+        star_counter += 2
+        space_counter -= 1
+    print(" " * (num - 1) + "*")
+
+
+# test code
 if __name__ == "__main__":
     spruce(5)
+    spruce(3)
