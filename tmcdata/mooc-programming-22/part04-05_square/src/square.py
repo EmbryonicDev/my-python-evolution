bf7be78e5f33b5179ecc8a0355bd25493979d842
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-03 15:34:20 +0100

    Complete exercise: square

--- tmcdata/mooc-programming-22/part04-05_square/src/square.py
+++ tmcdata/mooc-programming-22/part04-05_square/src/square.py
@@ -1,9 +1,19 @@
-# Copy here code of line function from previous exercise
+def line(num, text):
+    if text == "":
+        print("*" * num)
+    else:
+        print(text[0] * num)
+
 
 def square(size, character):
-    # You should call function line here with proper parameters
-    line(4, "x")
+    counter = size
+    while counter > 0:
+        line(size, character)
+        counter -= 1
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     square(5, "x")
+    print()
+    square(3, "o")
