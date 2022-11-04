eb0e534e864484496ecd38efd43315a964ebf4b4
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-04 18:55:51 +0100

    Complete exercise: list twice

--- tmcdata/mooc-programming-22/part04-16_list_twice/src/list_twice.py
+++ tmcdata/mooc-programming-22/part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,9 @@
-# Write your solution here
+items = []
+while True:
+    new_item = int(input("New item: "))
+    if new_item == 0:
+        break
+    items.append(new_item)
+    print(f"The list now: {items}")
+    print(f"The list in order: {sorted(items)} ")
+print("Bye!")
