691d82a7e8076eb112da2b9af9bd75a3b8aa3955
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-04 17:19:06 +0100

    Complete exercise: add items to list

--- tmcdata/mooc-programming-22/part04-13_add_items_to_list/src/add_items_to_list.py
+++ tmcdata/mooc-programming-22/part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,8 @@
-# Write your solution here
+item_count = int(input("How many items: "))
+counter = 1
+list = []
+while counter <= item_count:
+    value = int(input(f"Item {counter}: "))
+    list.append(value)
+    counter += 1
+print(list)
