54440be60f00ea110661fc284e25611bcaa279bd
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-04 18:18:44 +0100

    Complete exercise: add and remove from list

--- tmcdata/mooc-programming-22/part04-14_addition_and_removal/src/addition_and_removal.py
+++ tmcdata/mooc-programming-22/part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,14 @@
-# Write your solution here
+list = []
+number = 0
+while True:
+    print("The list is now", list)
+    action = input("a(d)d, (r)emove or e(x)it: ")
+    if action == 'x':
+        break
+    elif action == 'd':
+        number += 1
+        list.append(number)
+    elif action == 'r':
+        list.remove(number)
+        number -= 1
+print("Bye!")
