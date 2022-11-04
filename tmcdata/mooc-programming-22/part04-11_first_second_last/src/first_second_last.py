67f2d10f53011c30fdb691ffeb79324e3f290178
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-04 15:18:54 +0100

    Complete exercise: first second last

--- tmcdata/mooc-programming-22/part04-11_first_second_last/src/first_second_last.py
+++ tmcdata/mooc-programming-22/part04-11_first_second_last/src/first_second_last.py
@@ -1,7 +1,33 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def get_word(sentence, word_num):
+    counter = 0
+    while counter < word_num:
+        if " " in sentence:
+            index = sentence.find(' ')
+            word = sentence[0:index]
+            if " " in sentence:
+                sentence = sentence[index + 1:]
+        else:
+            word = sentence
+            break
+        counter += 1
+    return word
+
+
+def first_word(sentence):
+    return get_word(sentence, 1)
+
+
+def second_word(sentence):
+    return get_word(sentence, 2)
+
+
+def last_word(sentence):
+    return get_word(sentence, len(sentence))
+
+
+# test code
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
-    print(first_word(sentence))
-    print(second_word(sentence))
+    print(first_word("first second"))
+    print(second_word("first second"))
     print(last_word(sentence))
