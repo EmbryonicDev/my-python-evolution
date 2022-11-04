eddd450b9b9a87fe6d34b363f03445cef83a18a5
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-04 21:09:05 +0100

    Complete exercise: length of list

--- tmcdata/mooc-programming-22/part04-17_length_of_list/src/length_of_list.py
+++ tmcdata/mooc-programming-22/part04-17_length_of_list/src/length_of_list.py
@@ -1,5 +1,7 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def length(list):
+    return len(list)
+
+# test code
 if __name__ == "__main__":
     my_list = [3, 6, -4]
     result = length(my_list)
