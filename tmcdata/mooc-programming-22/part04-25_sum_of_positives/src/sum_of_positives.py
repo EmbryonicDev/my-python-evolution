e3f4d95b1d351bc6e041b873c4e4917a44aa1a02
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-05 14:56:43 +0100

    Complete exercise: sum of positives

--- tmcdata/mooc-programming-22/part04-25_sum_of_positives/src/sum_of_positives.py
+++ tmcdata/mooc-programming-22/part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def sum_of_positives(list):
+  result = 0
+  for i in list:
+    if i > 0:
+      result += i
+  return result
+  
+  
+if __name__ == "__main__":
+  my_list = [1, -2, 3, -4, 5]
+  result = sum_of_positives(my_list)
+  print("The result is", result)
