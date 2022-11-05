eabed873c12cdea2e068a783f65ddb7cda87b3e0
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-05 14:42:58 +0100

    Complete exercise: palindromes

--- tmcdata/mooc-programming-22/part04-24_palindromes/src/palindromes.py
+++ tmcdata/mooc-programming-22/part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,15 @@
-# Write your solution here
-# Note, that at this time the main program should not be written inside
-# if __name__ == "__main__":
-# block!
+def palindromes(word):
+  if word == word[::-1]:
+    return True
+  return False
+
+while True:
+  word = input("Please type in a palindrome: ")
+  if palindromes(word):
+    break
+  print("that wasn't a palindrome")
+print(word + " is a palindrome!")
+  
+
+if __name__ == "__main__":
+  palindromes("abcde")
