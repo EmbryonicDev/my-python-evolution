f476163b50750fbd83891c7d02f2d4b705b528fa
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 13:19:45 +0100

    Complete exercise: length of longest

--- tmcdata/mooc-programming-22/part04-29_length_of_longest/src/length_of_longest.py
+++ tmcdata/mooc-programming-22/part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def length_of_longest(list):
+  longest = len(list[0])
+  
+  for i in range(len(list) - 1):
+    if len(list[i+1]) > longest:
+      longest = len(list[i+1])
+      
+  return longest
+  
+
+if __name__ == "__main__":
+  my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+  result = length_of_longest(my_list)
+  print(result)
