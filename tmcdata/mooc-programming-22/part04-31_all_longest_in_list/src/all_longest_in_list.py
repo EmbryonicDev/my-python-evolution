d1d6310831fa45b8b40d94f47b80117a4c06a3d9
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 13:38:12 +0100

    Complete exercise: all the longest in list

--- tmcdata/mooc-programming-22/part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ tmcdata/mooc-programming-22/part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,19 @@
-# Write your solution here
+def all_the_longest(list):
+  new_list = [list[0]]
+  
+  for i in range(len(list) - 1):
+    if len(list[i+1]) > len(new_list[0]):
+      new_list = [list[i+1]]
+    elif len(list[i+1]) == len(new_list[0]):
+      new_list.append(list[i+1])
+  
+  return new_list
+
+if __name__ == "__main__":
+  my_list = ["first", "second", "fourth", "eleventh"]
+  result = all_the_longest(my_list)
+  print(result) # ['eleventh']
+  
+  my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+  result = all_the_longest(my_list)
+  print(result) # ['dorothy', 'richard']
