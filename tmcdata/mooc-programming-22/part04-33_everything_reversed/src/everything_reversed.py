3fabc1a24b3f89947186a71fd9cb1d0f61c10d5d
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 20:39:15 +0100

    Complete exercise: everything reversed

--- tmcdata/mooc-programming-22/part04-33_everything_reversed/src/everything_reversed.py
+++ tmcdata/mooc-programming-22/part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def everything_reversed(list):
+  new_list = []
+  for item in list:
+    new_list.append(item[::-1])
+  return new_list[::-1]
+  
+  
+if __name__ == "__main__":
+  my_list = ["Hi", "there", "example", "one more"]
+  new_list = everything_reversed(my_list)
+  print(new_list)
+  # ['erom eno', 'elpmaxe', 'ereht', 'iH']
