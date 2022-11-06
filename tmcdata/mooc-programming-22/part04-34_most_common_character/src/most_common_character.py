306ef65dfb877507c5b2ac695447ebfac1e8bb16
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 22:38:30 +0100

    Complete exercise: most common character

--- tmcdata/mooc-programming-22/part04-34_most_common_character/src/most_common_character.py
+++ tmcdata/mooc-programming-22/part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,18 @@
-# Write your solution here
+def most_common_character(string):
+  highest_count = ''
+  
+  for i in range(len(string)):
+    if highest_count == '':
+      highest_count = string[i]
+    elif string.count(string[i]) > string.count(highest_count):
+      highest_count = string[i]
+    
+  return highest_count
+    
+  
+if __name__ == "__main__":
+  first_string = "abcdbde"
+  print(most_common_character(first_string))
+
+  second_string = "exemplaryelementary"
+  print(most_common_character(second_string))
