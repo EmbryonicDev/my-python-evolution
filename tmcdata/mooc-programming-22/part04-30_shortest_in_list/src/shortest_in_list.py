7cc273f07a5c25a498a2b0fbefaa9cde65b1ede1
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 13:23:58 +0100

    Complete exercise: shortest in list

--- tmcdata/mooc-programming-22/part04-30_shortest_in_list/src/shortest_in_list.py
+++ tmcdata/mooc-programming-22/part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def shortest(list):
+  shortest = list[0]
+  
+  for i in range(len(list) - 1):
+    if len(list[i+1]) < len(shortest):
+      shortest = list[i+1]
+      
+  return shortest
+
+if __name__ == "__main__":
+  my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+  result = shortest(my_list)
+  print(result)
