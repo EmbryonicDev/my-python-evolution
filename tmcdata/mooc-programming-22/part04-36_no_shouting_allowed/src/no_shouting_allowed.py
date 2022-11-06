48b074852aa4c3a5214b9c141f9b3af5d5db18f3
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 22:56:39 +0100

    Complete exercise: no shouting allowed

--- tmcdata/mooc-programming-22/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ tmcdata/mooc-programming-22/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def no_shouting(list):
+  new_list = []
+  
+  for item in list:
+    if not item.isupper():
+      new_list.append(item)
+  
+  return new_list
+  
+  
+if __name__ == "__main__":
+  my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+  pruned_list = no_shouting(my_list)
+  print(pruned_list)
+  # Sample output
+  # ['def', 'lower', 'another lower', 'Capitalized']
