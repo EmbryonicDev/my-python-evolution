7c84be8b20a2b412e07f54d4366c0239e8f71dc6
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-06 22:50:39 +0100

    Complete exercise: no vowels allowed

--- tmcdata/mooc-programming-22/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ tmcdata/mooc-programming-22/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,15 @@
-# Write your solution here
+def no_vowels(string):
+  new_string = ''
+  vowels = "aeiou"
+  
+  for letter in string:
+    if letter not in vowels:
+      new_string += letter
+  
+  return new_string
+  
+if __name__ == "__main__":
+  my_string = "this is an example"
+  print(no_vowels(my_string))
+  # Sample output
+  # ths s n xmpl
