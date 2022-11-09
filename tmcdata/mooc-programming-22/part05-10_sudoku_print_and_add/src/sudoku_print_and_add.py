7395e8e8812c9cef74018fa54ce77d47e9f532f7
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-09 16:25:05 +0100

    Complete exercise: sudoku print & add

--- tmcdata/mooc-programming-22/part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
+++ tmcdata/mooc-programming-22/part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
@@ -1,1 +1,44 @@
-# Write your solution here
+def print_sudoku(sudoku: list):
+    c_count = 0
+
+    for row in sudoku:
+        b_count = 0
+        for cell in row:
+            b_count += 1
+            if cell == 0:
+                print("_", end=" ")
+            else:
+                print(cell, end=" ")
+            if b_count % 3 == 0 and b_count < 8:
+                print(" ", end="")
+        c_count += 1
+        if c_count % 3 == 0 and c_count < 8:
+            print()
+        print()
+
+
+def add_number(sudoku: list, row_no: int, column_no: int, number: int):
+    sudoku[row_no][column_no] = number
+
+
+if __name__ == "__main__":
+    sudoku = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    print_sudoku(sudoku)
+    add_number(sudoku, 0, 0, 2)
+    add_number(sudoku, 1, 2, 7)
+    add_number(sudoku, 5, 7, 3)
+    print()
+    print("Three numbers added:")
+    print()
+    print_sudoku(sudoku)
