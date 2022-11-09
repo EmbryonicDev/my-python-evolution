8e0d0379dee5a3560ba12a56389a806403928a85
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-09 16:24:20 +0100

    Complete exercise: remove smallest

--- tmcdata/mooc-programming-22/part05-09_remove_smallest/src/remove_smallest.py
+++ tmcdata/mooc-programming-22/part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def remove_smallest(numbers: list):
+    smallest_num = min(numbers)
+    numbers.remove(smallest_num)
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
