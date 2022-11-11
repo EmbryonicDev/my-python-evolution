0764f85d2a9c5f006f39379c17a423c5d339f261
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-11 20:43:06 +0100

    Complete exercise: tic tac toe

--- tmcdata/mooc-programming-22/part05-12_tic_tac_toe/src/tic_tac_toe.py
+++ tmcdata/mooc-programming-22/part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def play_turn(game_board: list, x: int, y: int, piece: str):
+    if (
+        0 <= x <= 2 and
+        0 <= y <= 2 and
+        game_board[y][x] == ''
+    ):
+        game_board[y][x] = piece
+        return True
+
+    return False
+
+
+if __name__ == "__main__":
+    game_board = [['', 'O', 'X'], ['', 'X', 'X'], ['X', 'X', '']]
+    print(play_turn(game_board, 0, 0, "X"))
+    print(game_board)
