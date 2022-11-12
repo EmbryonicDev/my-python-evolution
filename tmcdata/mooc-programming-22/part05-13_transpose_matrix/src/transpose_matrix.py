4dacf7499a19875444f4495fbe6cf14a5c067896
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-12 21:31:18 +0100

    Test & submit

--- tmcdata/mooc-programming-22/part05-13_transpose_matrix/src/transpose_matrix.py
+++ tmcdata/mooc-programming-22/part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,14 @@
-# Write your solution here
+
+def transpose(matrix: list):
+    length = len(matrix)
+    for i in range(length):
+        for j in range(i, length):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    transpose(matrix)
+    print(matrix)
