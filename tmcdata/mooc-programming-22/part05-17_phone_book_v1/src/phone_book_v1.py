4e2e1a42b7179d99cd2aec570da0ee7148bb2a63
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-14 15:16:13 +0100

    Complete exercise: phone book v1

--- tmcdata/mooc-programming-22/part05-17_phone_book_v1/src/phone_book_v1.py
+++ tmcdata/mooc-programming-22/part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,35 @@
-# Write your solution here
+def search(persons: dict):
+    name = input("name: ")
+    if name not in persons:
+      print("no number")
+    else:
+      print(persons[name])
+      
+def add(persons: dict):
+    name = input("name: ")
+    number = input("number: ")
+    persons[name] = number
+    print("ok!")
+      
+def main():
+  persons = {}
+  while True:
+    command = int(input("command (1 search, 2 add, 3 quit): "))
+    if command == 1:
+      search(persons)
+    if command == 2:
+      add(persons)
+    if command == 3:
+      break
+  print("quitting...")
+  
+main()
+    
+
+  
+    
+    
+    
+  
+  
+
