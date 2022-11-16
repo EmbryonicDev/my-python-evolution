96e9509c25112508a700b8edb9188b758bf9e8c7
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-16 15:33:03 +0100

    Complete exercise: invert dictionary

--- tmcdata/mooc-programming-22/part05-19_invert_dictionary/src/invert_dictionary.py
+++ tmcdata/mooc-programming-22/part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,23 @@
-# Write your solution here
+def invert(dictionary: dict):
+    # my solution
+    # temp_dict = dict(map(reversed, dictionary.items()))
+
+    # dictionary.clear()
+    # for key, value in temp_dict.items():
+    #     dictionary[key] = value
+
+    # model solution
+    copy = {}
+    for item in dictionary:
+        copy[item] = dictionary[item]
+
+    dictionary.clear()
+
+    for item in copy:
+        dictionary[copy[item]] = item
+
+
+if __name__ == "__main__":
+    s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+    invert(s)
+    print(s)
