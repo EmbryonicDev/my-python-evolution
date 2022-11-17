9b380232da9c74cb19e51db04cea128dd6f954da
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-17 12:37:42 +0100

    Complete exercise: create tuple

--- tmcdata/mooc-programming-22/part05-23_create_tuple/src/create_tuple.py
+++ tmcdata/mooc-programming-22/part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def create_tuple(x: int, y: int, z: int):
+  # function returns a tuple formed from parameters (smallest, greatest, sum)
+    temp_list = [x, y, z]
+    new_tuple = (min(temp_list), max(temp_list), sum(temp_list))
+    return new_tuple
+
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
