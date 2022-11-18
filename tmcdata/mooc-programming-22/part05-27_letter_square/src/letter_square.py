f58b1e669f80ed9766e7b4a86589b752111432ae
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-18 17:38:21 +0100

    Complete exercise: letter square

--- tmcdata/mooc-programming-22/part05-27_letter_square/src/letter_square.py
+++ tmcdata/mooc-programming-22/part05-27_letter_square/src/letter_square.py
@@ -1,1 +1,39 @@
-# Write your solution here
+layer = int(input("Layer: "))
+
+alpha = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+index = layer - 1
+my_range = index
+level = 1
+half_list = [alpha[index] * layer]
+
+# create first half of each required string & push to list
+for i in range(index):
+    temp_str = half_list[-1][0:level]
+
+    for i in range(my_range):
+        temp_str += alpha[index - level]
+    half_list.append(temp_str)
+
+    level += 1
+    my_range -= 1
+
+# create reversed version of half_list & remove first letter
+reversed_half_list = []
+for item in half_list:
+    item = item[0:-1]
+    reversed_half_list.append(item[::-1])
+
+# combine half list & reverse list for final str
+combined_list = []
+for i in range(0, layer):
+    temp_str = half_list[i] + reversed_half_list[i]
+    combined_list.append(temp_str)
+
+# prepare combined list for final submission
+  # append combined list with reversed order of combined list (except last index)
+for i in range(index - 1, -1, -1):
+    combined_list.append(combined_list[i])
+
+# print combined list values
+for string in combined_list:
+    print(string)
