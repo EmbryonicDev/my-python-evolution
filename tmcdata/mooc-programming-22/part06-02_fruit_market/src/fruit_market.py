f864fe0d6c588ba645f0761eed088116346d0d1b
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-18 18:41:16 +0100

    Complete exercise: fruit market

--- tmcdata/mooc-programming-22/part06-02_fruit_market/src/fruit_market.py
+++ tmcdata/mooc-programming-22/part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,13 @@
-# write your solution here
+def read_fruits():
+    with open('fruits.csv') as new_file:
+        fruit_prices = {}
+        for line in new_file:
+            # split line to 2 pieces
+            parts = line.split(';')
+            # name first, price second
+            fruit_prices[parts[0]] = float(parts[1])
+    return fruit_prices
+
+
+if __name__ == "__main__":
+    print(read_fruits())
