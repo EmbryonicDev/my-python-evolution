ee1e5841b510a8b9f41c86838d1071a8c2689e23
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-18 18:11:13 +0100

    Part 6 - Reading Files - Complete exercise: Largest number

--- tmcdata/mooc-programming-22/part06-01_largest_number/src/largest_number.py
+++ tmcdata/mooc-programming-22/part06-01_largest_number/src/largest_number.py
@@ -1,2 +1,11 @@
-# write your solution here
+def largest():
+    number_list = []
+    with open('numbers.txt') as new_file:
+        for line in new_file:
+            line = line.replace('\n', '')
+            number_list.append(line)
+    return int(max(number_list))
 
+
+if __name__ == "__main__":
+    largest()
