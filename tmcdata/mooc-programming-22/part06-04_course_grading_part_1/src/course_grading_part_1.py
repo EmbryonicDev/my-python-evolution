e0ab9f74d5ffc37b51666e022f641e35ae03b310
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-22 15:14:44 +0100

    Complete exercise: course grading part 1

--- tmcdata/mooc-programming-22/part06-04_course_grading_part_1/src/course_grading_part_1.py
+++ tmcdata/mooc-programming-22/part06-04_course_grading_part_1/src/course_grading_part_1.py
@@ -1,1 +1,29 @@
-# write your solution here
+
+student_info = input("Student information: ")
+exercise_data = input("Exercises completed: ")
+
+students1 = {}
+with open(student_info) as file:
+    for line in file:
+        parts = line.split(';')
+        if parts[0] == 'id':
+            continue
+        students1[int(parts[0])] = parts[1] + " " + \
+            parts[2].replace('\n', '')
+
+exercises1 = {}
+with open(exercise_data) as file:
+    for line in file:
+        line = line.replace('\n', '')
+        parts = line.split(';')
+        if parts[0] == 'id':
+            continue
+        # convert to int
+        for i in range(0, len(parts)):
+            parts[i] = int(parts[i])
+        exercises1[int(parts[0])] = parts[1:]
+
+for id, student in students1.items():
+    if id in exercises1:
+        my_sum = sum(exercises1[id])
+        print(f"{student} {my_sum}")
