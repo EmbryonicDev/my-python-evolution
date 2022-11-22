d219e03293b27d1329b42d0d9fd82ac6ce8d271e
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-22 17:00:23 +0100

    Complete exercise: spellchekcer

--- tmcdata/mooc-programming-22/part06-07_spellchecker/src/spellchecker.py
+++ tmcdata/mooc-programming-22/part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,13 @@
-# write your solution here
+text = input('Write text: ')
+
+dictionary = []
+with open('wordlist.txt') as file:
+    for line in file:
+        dictionary.append(line.strip())
+
+text_list = text.split(' ')
+for word in text_list:
+    if word.lower() in dictionary:
+        print(word, end=' ')
+    else:
+        print(f"*{word}*", end=' ')
