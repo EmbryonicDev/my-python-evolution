bcdcc4edc57178baa238d6e174eff59073dde468
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-24 15:54:27 +0100

    Complete exercise: inscription

--- tmcdata/mooc-programming-22/part06-10_inscription/src/inscription.py
+++ tmcdata/mooc-programming-22/part06-10_inscription/src/inscription.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def main():
+    name = input("Whom should i sign this to: ")
+    save_location = input("Where shall I save it: ")
+
+    with open(save_location, 'w') as file:
+        file.write(
+            f"Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
+
+
+main()
