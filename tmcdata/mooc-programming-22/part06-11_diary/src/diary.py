9eb86a20659fbf1c977fdaf7c15205848686eea7
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-24 16:22:52 +0100

    Complete exercise: diary

--- tmcdata/mooc-programming-22/part06-11_diary/src/diary.py
+++ tmcdata/mooc-programming-22/part06-11_diary/src/diary.py
@@ -1,1 +1,25 @@
-# Write your solution here
+def add_entry(entry: str):
+    with open('diary.txt', 'a') as diary:
+        diary.write(entry + '\n')
+
+
+def read_entry():
+    with open('diary.txt') as diary:
+        for line in diary:
+            print(line.strip())
+
+
+while True:
+    print('1 - add an entry, 2 - read entries, 0 - quit')
+    function = int(input('Function: '))
+    if function == 0:
+        break
+    elif function == 1:
+        entry = input('Diary entry: ')
+        add_entry(entry)
+        print('Diary saved')
+    elif function == 2:
+        print('Entries:')
+        read_entry()
+
+print('Bye now!')
