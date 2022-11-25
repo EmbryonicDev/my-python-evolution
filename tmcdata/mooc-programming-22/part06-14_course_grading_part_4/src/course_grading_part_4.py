b6dcdfdbf44b60ffcea2d96ca3b6cc66f9acfcee
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-25 20:02:17 +0100

    Complete exercise: course grading part 4

--- tmcdata/mooc-programming-22/part06-14_course_grading_part_4/src/course_grading_part_4.py
+++ tmcdata/mooc-programming-22/part06-14_course_grading_part_4/src/course_grading_part_4.py
@@ -2,6 +2,7 @@ import math
 student_info = input("Student information: ")
 exercise_data = input("Exercises completed: ")
 exam_points = input("Exam points: ")
+course_information = input("Course information: ")
 
 
 def convert_to_int(parts: list):
@@ -37,6 +38,12 @@ with open(exam_points) as file:
         convert_to_int(parts)
         points[parts[0]] = parts[1:]
 
+course_info = {}
+with open(course_information) as file:
+    for line in file:
+        parts = line.split(':')
+        course_info[parts[0]] = parts[1]
+
 
 def get_points(all_points: int):
     borders = [15, 18, 20, 24, 28, 100]
@@ -47,12 +54,25 @@ def get_points(all_points: int):
         grade += 1
 
 
-print(f"{'name':30}{'exec_nbr':10}{'exec_pts.':10}{'exm_pts.':10}{'tot_pts.':10}{'grade':10} ")
-for id, student in students1.items():
-    if id in exercises1:
+with open('results.csv', 'w') as csv_file, open('results.txt', 'w') as txt_file:
+    # print txt file header
+    header = f"{course_info['name'].strip()}, {course_info['study credits'].strip()} credits"
+    txt_file.write(header+'\n')
+    txt_file.write('='*len(header)+'\n')
+    txt_file.write(
+        f"{'name':30}{'exec_nbr':10}{'exec_pts.':10}{'exm_pts.':10}{'tot_pts.':10}{'grade':10}\n")
+
+    # define variables
+    for id, student in students1.items():
         exercises = sum(exercises1[id])
         exercise_points = math.floor(sum(exercises1[id])/4)
         student_points = sum(points[id])
         total_points = student_points + exercise_points
         grade = get_points(total_points)
-        print(f"{student:30}{exercises:<10}{exercise_points:<10}{student_points:<10}{total_points:<10}{grade:<10}")
+
+        # write student data to txt file
+        txt_file.write(
+            f"{student:30}{exercises:<10}{exercise_points:<10}{student_points:<10}{total_points:<10}{grade:<10}\n")
+
+        # write student data to csv file
+        csv_file.write(f"{id};{student};{grade}\n")
