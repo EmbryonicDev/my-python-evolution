3419eb3db0656f6b82f4e178908182c85949a632
EmbryonicDev <embryonic.dev@gmail.com>
2022-11-28 13:30:40 +0100

    Complete exercise: parameter validation

--- tmcdata/mooc-programming-22/part06-18_parameter_validation/src/parameter_validation.py
+++ tmcdata/mooc-programming-22/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,17 @@
-# Write your solution here
+def new_person(name: str, age: int):
+    # validate name
+    if (len(name) > 40 or len(name) < 1 or
+        " " not in name):
+        raise ValueError("Invalid argument value for name: " + name)
+    
+    # validate age
+    if age < 0 or age > 150:
+        raise ValueError("Invalid argument value for age: " + str(age))
+    
+    # no error:
+    return (name, age)
+
+  
+if __name__ == "__main__":
+  print(new_person('Andrew', 32))
+  
