71b4a5d3f2f7066fc831a5b95855176fe3cd2dcb
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-28 12:13:37 +0100

    Complete exercise: read input

--- tmcdata/mooc-programming-22/part06-17_read_input/src/read_input.py
+++ tmcdata/mooc-programming-22/part06-17_read_input/src/read_input.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def read_input(user_prompt: str, min_num: int, max_num: int):
+    while True:
+        try:
+            number = int(input(f"{user_prompt}"))
+            if min_num < number < max_num:
+                return number
+        except ValueError:
+            pass
+        print(f"You must type in an integer between {min_num} and {max_num}")
+
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
