26dd99dc824c3b50f9cd667229203af24ac9cca6
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-29 18:21:21 +0100

    Complete exercise: password generator part 1

--- tmcdata/mooc-programming-22/part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ tmcdata/mooc-programming-22/part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,14 @@
-# Write your solution here
+from random import choice
+from string import ascii_lowercase
+
+
+def generate_password(psw_count: int):
+    psw = ''
+    for i in range(psw_count):
+        psw += choice(ascii_lowercase)
+    return psw
+
+
+if __name__ == '__main__':
+    for i in range(10):
+        print(generate_password(12))
