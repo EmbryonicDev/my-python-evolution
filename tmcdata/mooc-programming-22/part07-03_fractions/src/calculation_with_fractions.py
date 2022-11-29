7edb78a80437e8e0d4c6f4a8870cbb0611e9aa21
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-29 15:57:57 +0100

    Complete exercise: calc with fractions

--- tmcdata/mooc-programming-22/part07-03_fractions/src/calculation_with_fractions.py
+++ tmcdata/mooc-programming-22/part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,15 @@
-# Write your solution here
+from fractions import Fraction
+
+
+def fractionate(amount: int):
+    list = []
+    for i in range(amount):
+        list.append(Fraction(1, amount))
+    return list
+
+
+if __name__ == '__main__':
+    for p in fractionate(3):
+        print(p)
+    print()
+    print(fractionate(5))
