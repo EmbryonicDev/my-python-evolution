73f48993f0d3d322f0e1b0e3a7605667d8c7e2cd
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-30 17:30:06 +0100

    Complete exercise: random words

--- tmcdata/mooc-programming-22/part07-08_random_words/src/random_words.py
+++ tmcdata/mooc-programming-22/part07-08_random_words/src/random_words.py
@@ -1,1 +1,22 @@
-# Write your solution here
+from random import sample
+
+
+def words(n: int, beginning: str):
+    matching_words = []
+    with open('words.txt') as file:
+        for line in file:
+            if line.strip().startswith(beginning):
+                matching_words.append(line.strip())
+
+    if len(matching_words) < n:
+        error = f"I did not find {n} words that start with '{beginning}', only {len(matching_words)}"
+        raise ValueError(error)
+
+    return sample(matching_words, n)
+
+
+if __name__ == '__main__':
+    print()
+    print(words(3, 'catn'))
+    print()
+    print(words(30, 'catn'))
