dfc589d4210082425ad974bc73c1c779436f7ef1
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-30 20:04:20 +0100

    Complete exercise: valid pic

--- tmcdata/mooc-programming-22/part07-10_valid_pic/src/valid_pic.py
+++ tmcdata/mooc-programming-22/part07-10_valid_pic/src/valid_pic.py
@@ -1,1 +1,33 @@
-# Write your solution here
+from datetime import datetime
+
+
+def is_it_valid(pic: str):
+    if len(pic) != 11:
+        return False
+
+    control_string = '0123456789ABCDEFHJKLMNPRSTUVWXY'
+    control_char = pic[-1]
+    integer = int(pic[0:6] + pic[7:-1])
+    index = integer % 31
+
+    if pic[6] == '+':
+        year = int('18' + pic[4:6])
+    elif pic[6] == '-':
+        year = int('19' + pic[4:6])
+    elif pic[6] == 'A':
+        year = int('20' + pic[4:6])
+
+    try:
+        date = datetime(year, int(pic[2:4]), int(pic[0:2]))
+    except ValueError:
+        return False
+
+    return control_char == control_string[index]
+
+
+if __name__ == '__main__':
+    print(is_it_valid('290200-1239'))
+    print(is_it_valid('230827-906F'))
+    print(is_it_valid('120488+246L'))
+    print(is_it_valid('310823A9877'))
+    print(is_it_valid('230827-906F1'))
