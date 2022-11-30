cc4c0b65c78bb669e01226b36a41690e3ed66e74
djdevlam-apple <embryonic.dev@gmail.com>
2022-11-30 18:13:41 +0100

    Complete exercise: how old

--- tmcdata/mooc-programming-22/part07-09_how_old/src/how_old.py
+++ tmcdata/mooc-programming-22/part07-09_how_old/src/how_old.py
@@ -1,1 +1,15 @@
-# Write your solution here
+from datetime import datetime, timedelta
+
+day = int(input('Day: '))
+month = int(input('Month: '))
+year = int(input('Year: '))
+
+birthdate = datetime(year, month, day)
+new_millennium = datetime(1999, 12, 31)
+
+if new_millennium > birthdate:
+    days_old = new_millennium - birthdate
+    print(
+        f"You were {days_old.days} days old on the eve of the new millennium.")
+else:
+    print(f"You weren't born yet on the eve of the new millennium.")
