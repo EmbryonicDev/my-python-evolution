de5efaea06103e3a2602430eb0ae74b4d33be93a
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-02 17:38:34 +0100

    Complete exercise: handling JSON files

--- tmcdata/mooc-programming-22/part07-12_json_files/src/json_files.py
+++ tmcdata/mooc-programming-22/part07-12_json_files/src/json_files.py
@@ -1,1 +1,26 @@
-# Write your solution here
+import json
+
+
+def print_persons(filename: str):
+    with open(filename) as file:
+        data = file.read()
+    people = json.loads(data)
+    for person in people:
+        hobbies = ", ".join(person['hobbies'])
+        print(
+            f"{person['name']} {person['age']} years ({hobbies})")
+
+
+if __name__ == '__main__':
+    print('File 1')
+    print_persons('file1.json')
+    print()
+    print('File 2')
+    print_persons('file2.json')
+    print()
+    print('File 3')
+    print_persons('file3.json')
+    print()
+    print('File 4')
+    print_persons('file4.json')
+    print()
