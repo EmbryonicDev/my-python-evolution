c091298c0b10892d39f6b77b4c7302fa57acb26f
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-05 20:34:58 +0100

    Complete exercise: string helper

--- tmcdata/mooc-programming-22/part07-17_string_helper/src/string_helper.py
+++ tmcdata/mooc-programming-22/part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,33 @@
-# Write your solution here
+from string import ascii_letters, digits
+
+
+def change_case(org_string: str):
+    new_string = ''
+    for letter in org_string:
+        if letter.isupper():
+            new_string += letter.lower()
+        else:
+            new_string += letter.upper()
+    return new_string
+
+
+def split_in_half(orig_string: str):
+    split_at = int(len(orig_string) / 2)
+    return orig_string[0:split_at], orig_string[split_at:]
+
+
+def remove_special_characters(orig_string: str):
+    new_string = ''
+    allowed = ascii_letters + digits + ' '
+    for char in orig_string:
+        if char in allowed:
+            new_string += char
+    return new_string
+
+
+if __name__ == '__main__':
+    print(change_case('dEwAlD'))
+    print()
+    print(split_in_half('123456789'))
+    print()
+    print(remove_special_characters("This is a test, lets see how it goes!!!11!"))
