44865176f492d146ff32423d5f7d9df8e7be1d86
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-05 18:28:31 +0100

    Complete exercise: who cheated2: 2nd attempt

--- tmcdata/mooc-programming-22/part07-15_who_cheated_2/src/who_cheated_2 copy.py
+++ tmcdata/mooc-programming-22/part07-15_who_cheated_2/src/who_cheated_2 copy.py
@@ -4,7 +4,6 @@ import csv
 
 def final_points():
     students = {}
-    final_points = {}
 
     with open('start_times.csv') as times_file, open('submissions.csv') as submissions_file:
 
@@ -12,67 +11,36 @@ def final_points():
         for line in csv.reader(times_file, delimiter=';'):
             name = line[0]
             start_time = datetime.strptime(line[1], '%H:%M')
-            students[name] = {'start_time': start_time, 'tasks': []}
-        # print(students)
-        # students.append({'name': name, "start_time": start_time, 'tasks': []})
+            students[name] = {'start_time': start_time, 'tasks': {}}
 
-        # get all submission:
+        # get all submission data to update students dict
         for line in csv.reader(submissions_file, delimiter=';'):
             name = line[0]
-            task = line[1]
-            points = int(line[2])
+            task_num = int(line[1])
+            task_points = int(line[2])
             end_time = datetime.strptime(line[3], "%H:%M")
 
-            # for student in students:
             for student in students:
                 if name == student:
-                    students[name]['end_time'] = end_time
-                    if students[name]['end_time'] - students[name]['start_time'] < timedelta(hours=3):
-                        students[name]['tasks'].append({task, points})
-            # print(students)
+                    students[student]['end_time'] = end_time
+                    # add tasks only if end time is less than 3 hours from start
+                    if students[student]['end_time'] - students[student]['start_time'] < timedelta(hours=3):
+                        # add task to tasks if task num not found
+                        if task_num not in students[student]['tasks']:
+                            students[student]['tasks'][task_num] = task_points
+                        # if task num found, only add if new value is greater than old value
+                        else:
+                            for key, value in students[student]['tasks'].items():
+                                if key == task_num and task_points > value:
+                                    students[student]['tasks'][task_num] = task_points
 
+        # get sum of all task points & add to final_points dict
+        final_points = {}
         for student in students:
-            print(student)
-            filtered_points = {}
-            for i in range(1, 9):
-                filtered_points[i] = 0
-
-            for index, value in filtered_points.items():
-                highest_value = value
-
-        #         for tasks in students[student]['tasks']:
-        #             print(tasks[1], index)
-            # if int(task[0]) == index and task[1] > highest_value:
-            #     print('hello')
-            #         if int(task[0]) == index and task[1] > highest_value:
-            #             highest_value = task[1]
-            #         filtered_points[index] = highest_value
-            # students[name]['final_points'] = filtered_points
-
-            # sum_points = 0
-            # for key, value in students[name]['final_points'].items():
-            #     sum_points += value
-            # final_points[name] = sum_points
-
-            # print(students)
-
-    # for student in students:
-    #     filtered_points = {}
-    #     for i in range(1, 9):
-    #         filtered_points[i] = 0
-
-    #     for task_key, task_value in filtered_points.items():
-    #         highest_value = task_value
-    #         for task in student['tasks']:
-    #             if int(task[0]) == task_key and task[1] > highest_value:
-    #                 highest_value = task[1]
-    #             filtered_points[task_key] = highest_value
-    #     student['final_points'] = filtered_points
-
-    #     sum_points = 0
-    #     for key, value in student['final_points'].items():
-    #         sum_points += value
-    #     final_points[student['name']] = sum_points
+            sum_points = 0
+            for key, value in students[student]['tasks'].items():
+                sum_points += value
+            final_points[student] = sum_points
 
     return final_points
 
