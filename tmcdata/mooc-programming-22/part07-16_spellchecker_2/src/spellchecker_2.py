9ba68cb924d49668b605bda50222aca0ae769513
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-05 19:47:22 +0100

    Complete exercise: spellchecker 2

--- tmcdata/mooc-programming-22/part07-16_spellchecker_2/src/spellchecker_2.py
+++ tmcdata/mooc-programming-22/part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,19 @@
-# Write your solution here
+from difflib import get_close_matches
+
+dictionary = []
+with open('wordlist.txt') as file:
+    for line in file:
+        dictionary.append(line.strip())
+
+text = input('Write text: ')
+suggestions = {}
+for word in text.split(' '):
+    if word.lower() in dictionary:
+        print(word, end=' ')
+    else:
+        print(f"*{word}*", end=' ')
+        suggestions[word] = get_close_matches(word, dictionary)
+
+print('\nsuggestions:')
+for key, value in suggestions.items():
+    print(f"{key}: {', '.join(value)}")
