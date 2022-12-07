e2a8f58a7952bf26d2fb59ac36b1df543321dbbe
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-07 19:38:00 +0100

    Complete exercise: shopping list

--- tmcdata/mooc-programming-22/part08-04_shopping_list/src/shopping_list.py
+++ tmcdata/mooc-programming-22/part08-04_shopping_list/src/shopping_list.py
@@ -16,6 +16,28 @@ class ShoppingList:
     def amount(self, n: int):
         return self.products[n - 1][1]
 
+
 # -------------------------
 # Write your solution here:
 # -------------------------
+
+def total_units(my_list: ShoppingList):
+    total_units = 0
+    for i in range(my_list.number_of_items()):
+        total_units += my_list.amount(i)
+    return total_units
+
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("pineapple", 1)
+
+    print(my_list.number_of_items())
+    print(my_list.item(1))
+    print(my_list.amount(1))
+    print(my_list.item(2))
+    print(my_list.amount(2))
+
+    print(total_units(my_list))
