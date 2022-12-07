9b9bc2b1a84af74aa4c22d0926911c272e9e1ca5
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-07 18:35:13 +0100

    Complete exercise: smallest average

--- tmcdata/mooc-programming-22/part08-01_smallest_average/src/smallest_average.py
+++ tmcdata/mooc-programming-22/part08-01_smallest_average/src/smallest_average.py
@@ -1,1 +1,22 @@
-# Write your solution here
+def smallest_average(person1: dict, person2: dict, person3: dict):
+    persons = [person1, person2, person3]
+    min_points = 9999999
+    dumbest = ''
+
+    for person in persons:
+        total = 0
+        for key, value in person.items():
+            if 'result' in key:
+                total += value
+        if total < min_points:
+            min_points = total
+            dumbest = person
+    return dumbest
+
+
+if __name__ == '__main__':
+    person1 = {"name": "Mary", "result1": 9, "result2": 9, "result3": 9}
+    person2 = {"name": "Gary", "result1": 7, "result2": 7, "result3": 7}
+    person3 = {"name": "Larry", "result1": 8, "result2": 8, "result3": 8}
+
+    print(smallest_average(person1, person2, person3))
