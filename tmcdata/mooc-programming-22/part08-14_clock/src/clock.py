94e1a1aff8e0f1ec1d4d2ddb5a0e233e9963597c
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-08 18:34:15 +0100

    Complete exercise: clock

--- tmcdata/mooc-programming-22/part08-14_clock/src/clock.py
+++ tmcdata/mooc-programming-22/part08-14_clock/src/clock.py
@@ -1,1 +1,49 @@
-# Write your solution here:
+from datetime import datetime, time
+
+
+class Clock:
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def tick(self):
+        self.seconds += 1
+        if self.seconds == 60:
+            self.seconds = 0
+            self.minutes += 1
+        if self.minutes == 60:
+            self.minutes = 0
+            self.hours += 1
+        if self.hours == 24:
+            self.hours = 0
+
+    def set(self, hours: int, minutes: int, seconds=0):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def __str__(self):
+        my_time = time(hour=self.hours, minute=self.minutes,
+                       second=self.seconds)
+        return f"{my_time}"
+
+
+if __name__ == '__main__':
+    clock = Clock(23, 59, 55)
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+
+    clock.set(12, 5)
+    print(clock)
