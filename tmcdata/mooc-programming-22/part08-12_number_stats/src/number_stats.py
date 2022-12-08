3c20f77e93063532995b8262456b173f8c4e5012
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-08 17:09:21 +0100

    Complete exercise: number stats

--- tmcdata/mooc-programming-22/part08-12_number_stats/src/number_stats.py
+++ tmcdata/mooc-programming-22/part08-12_number_stats/src/number_stats.py
@@ -1,10 +1,40 @@
 # Write your solution here!
-class  NumberStats:
+class NumberStats:
     def __init__(self):
-        self.numbers = 0
+        self.numbers = []
 
-    def add_number(self, number:int):
-        pass
+    def add_number(self, number: int):
+        self.numbers.append(number)
 
     def count_numbers(self):
-        pass
+        return len(self.numbers)
+
+    def get_sum(self):
+        return sum(self.numbers)
+
+    def average(self):
+        my_sum = sum(self.numbers)
+        length = len(self.numbers)
+        return my_sum / length if length > 0 else 0
+
+
+def main():
+    results = NumberStats()
+    even_numbers = NumberStats()
+    odd_numbers = NumberStats()
+    print('Please type in integer numbers:')
+    while True:
+        number = int(input())
+        if number == - 1:
+            break
+        results.add_number(number)
+        even_numbers.add_number(
+            number) if number % 2 == 0 else odd_numbers.add_number(number)
+
+    print(f"Sum of numbers: {results.get_sum()}")
+    print(f"Mean of numbers: {results.average()}")
+    print(f"Sum of even numbers: {even_numbers.get_sum()}")
+    print(f"Sum of odd numbers: {odd_numbers.get_sum()}")
+
+
+main()
