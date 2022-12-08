313441ed877ba198ad3159e69d3ff0c56a84079c
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-08 14:16:41 +0100

    Complete exercise: decrease counter

--- tmcdata/mooc-programming-22/part08-10_decreasing_counter/src/decreasing_counter.py
+++ tmcdata/mooc-programming-22/part08-10_decreasing_counter/src/decreasing_counter.py
@@ -2,11 +2,47 @@
 class DecreasingCounter:
     def __init__(self, initial_value: int):
         self.value = initial_value
+        self.reset_value = initial_value
 
     def print_value(self):
         print("value:", self.value)
 
     def decrease(self):
-        pass
+        if self.value > 0:
+            self.value -= 1
 
-    # Write the rest of the methods here!
+    def set_to_zero(self):
+        self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.reset_value
+
+
+if __name__ == '__main__':
+    # part 1
+    counter = DecreasingCounter(10)
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+
+    # part 2
+    counter = DecreasingCounter(2)
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+
+    # part 3
+    counter = DecreasingCounter(55)
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.print_value()
+    counter.reset_original_value()
+    counter.print_value()
