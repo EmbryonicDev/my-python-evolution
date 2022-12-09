2f87c2ed5eca78fb53be921e1dad25049641dc01
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-09 19:15:28 +0100

    Complete exercise: shortest in room: part 2

--- tmcdata/mooc-programming-22/part09-08_shortest_in_room/src/shortest_in_room.py
+++ tmcdata/mooc-programming-22/part09-08_shortest_in_room/src/shortest_in_room.py
@@ -28,8 +28,18 @@ class Room:
         for person in self.people_in_room:
             print(person)
 
+    def shortest(self):
+        shortest_person = ''
+        shortest_height = 999
+        for person in self.people_in_room:
+            if person.height < shortest_height:
+                shortest_height = person.height
+                shortest_person = person
+        return None if len(self.people_in_room) == 0 else shortest_person
+
 
 if __name__ == "__main__":
+    print("\nPart1")
     room = Room()
     print("Is the room empty?", room.is_empty())
     room.add(Person("Lea", 183))
@@ -38,4 +48,18 @@ if __name__ == "__main__":
     room.add(Person("Nina", 162))
     room.add(Person("Dorothy", 155))
     print("Is the room empty?", room.is_empty())
+    room.print_contents()
+
+    print("\nPart2")
+    room = Room()
+    print("Is the room empty?", room.is_empty())
+    print("Shortest:", room.shortest())
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Nina", 162))
+    room.add(Person("Ally", 166))
+    print()
+    print("Is the room empty?", room.is_empty())
+    print("Shortest:", room.shortest())
+    print()
     room.print_contents()
