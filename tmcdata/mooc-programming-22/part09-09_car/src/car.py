c955ef6b1c47388c98ba0d41e851b4585970f8e9
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-09 21:01:36 +0100

    Complete exercise: car

--- tmcdata/mooc-programming-22/part09-09_car/src/car.py
+++ tmcdata/mooc-programming-22/part09-09_car/src/car.py
@@ -1,1 +1,32 @@
-# WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__petrol = 0
+        self.__odometer = 0
+
+    def fill_up(self):
+        self.__petrol = 60
+
+    def drive(self, km: int):
+        for i in range(km):
+            if self.__petrol > 0:
+                self.__petrol -= 1
+                self.__odometer += 1
+
+    def __str__(self):
+        return f"Car: odometer reading {self.__odometer} km, petrol remaining {self.__petrol} litres"
+
+
+if __name__ == "__main__":
+    car = Car()
+    print(car)
+    car.fill_up()
+    print(car)
+    car.drive(20)
+    print(car)
+    car.drive(50)
+    print(car)
+    car.drive(10)
+    print(car)
+    car.fill_up()
+    car.fill_up()
+    print(car)
