7e159594ecedcf8e368b9e55c88c58b453950066
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-09 18:44:38 +0100

    Complete exercise: shortes in room: part 1

--- tmcdata/mooc-programming-22/part09-08_shortest_in_room/src/shortest_in_room.py
+++ tmcdata/mooc-programming-22/part09-08_shortest_in_room/src/shortest_in_room.py
@@ -5,4 +5,37 @@ class Person:
         self.height = height
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.height} cm)"
+
+
+class Room:
+    def __init__(self):
+        self.people_in_room = []
+
+    def add(self, person: Person):
+        self.people_in_room.append(person)
+
+    def is_empty(self):
+        return len(self.people_in_room) == 0
+
+    def print_contents(self):
+        count = len(self.people_in_room)
+        total_height = 0
+        for person in self.people_in_room:
+            total_height += person.height
+        print(
+            f"There are {count} persons in the room, and their combined height is {total_height} cm")
+        for person in self.people_in_room:
+            print(person)
+
+
+if __name__ == "__main__":
+    room = Room()
+    print("Is the room empty?", room.is_empty())
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Ally", 166))
+    room.add(Person("Nina", 162))
+    room.add(Person("Dorothy", 155))
+    print("Is the room empty?", room.is_empty())
+    room.print_contents()
