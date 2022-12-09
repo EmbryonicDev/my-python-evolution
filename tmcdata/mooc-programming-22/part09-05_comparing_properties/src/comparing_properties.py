60a352fee0792bd777fee439b6bb2ac10ea39dd2
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-09 16:38:47 +0100

    Complete exercise: comparing properties: part 2

--- tmcdata/mooc-programming-22/part09-05_comparing_properties/src/comparing_properties.py
+++ tmcdata/mooc-programming-22/part09-05_comparing_properties/src/comparing_properties.py
@@ -9,11 +9,26 @@ class RealProperty:
     def bigger(self, compared_to: 'RealProperty'):
         return self.square_metres > compared_to.square_metres
 
+    def price_difference(self, compared_to: 'RealProperty'):
+        self_price = self.square_metres * self.price_per_sqm
+        compared_to_price = compared_to.square_metres * compared_to.price_per_sqm
+        difference = self_price - compared_to_price
+        return difference if difference > 0 else difference * -1
+
 
 if __name__ == '__main__':
+    print('\nPart 1')
     central_studio = RealProperty(1, 16, 5500)
     downtown_two_bedroom = RealProperty(2, 38, 4200)
     suburbs_three_bedroom = RealProperty(3, 78, 2500)
 
     print(central_studio.bigger(downtown_two_bedroom))
     print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
+
+    print('\nPart 2')
+    central_studio = RealProperty(1, 16, 5500)
+    downtown_two_bedroom = RealProperty(2, 38, 4200)
+    suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
+    print(central_studio.price_difference(downtown_two_bedroom))
+    print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
