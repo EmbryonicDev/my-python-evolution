2600f4c47325c8f3a6f9c9ed420ebda7e6ff7bdd
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-09 18:00:02 +0100

    Complete exercise: box of presents: part 1

--- tmcdata/mooc-programming-22/part09-07_box_of_presents/src/box_of_presents.py
+++ tmcdata/mooc-programming-22/part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,15 @@
-# WRITE YOUR SOLUTION HERE:
+class Present:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f"Preset: {self.name} ({self.weight} kg)"
+
+
+if __name__ == '__main__':
+    print('\nPart 1')
+    book = Present("ABC Book", 2)
+    print("The name of the present:", book.name)
+    print("The weight of the present:", book.weight)
+    print("Present:", book)
