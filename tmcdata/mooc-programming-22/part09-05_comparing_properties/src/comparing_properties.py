068be360d0b5009b3bdb5d8fc6b0d6900cd2d07b
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-09 16:46:40 +0100

    Complete exercise: comparing properties: part 3

--- tmcdata/mooc-programming-22/part09-05_comparing_properties/src/comparing_properties.py
+++ tmcdata/mooc-programming-22/part09-05_comparing_properties/src/comparing_properties.py
@@ -15,6 +15,11 @@ class RealProperty:
         difference = self_price - compared_to_price
         return difference if difference > 0 else difference * -1
 
+    def more_expensive(self, compared_to: 'RealProperty'):
+        self_price = self.square_metres * self.price_per_sqm
+        compared_to_price = compared_to.square_metres * compared_to.price_per_sqm
+        return self_price > compared_to_price
+
 
 if __name__ == '__main__':
     print('\nPart 1')
@@ -32,3 +37,11 @@ if __name__ == '__main__':
 
     print(central_studio.price_difference(downtown_two_bedroom))
     print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
+
+    print('\nPart 3')
+    central_studio = RealProperty(1, 16, 5500)
+    downtown_two_bedroom = RealProperty(2, 38, 4200)
+    suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
+    print(central_studio.more_expensive(downtown_two_bedroom))
+    print(suburbs_three_bedroom.more_expensive(downtown_two_bedroom))
