09da00abc76e2f8b00d4a0b6ac95203b58e032f2
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-15 20:24:10 +0100

    Complete exercise: service charge

--- tmcdata/mooc-programming-22/part09-12_service_charge/src/service_charge.py
+++ tmcdata/mooc-programming-22/part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,34 @@
-# WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self, name: str, acc_num: str, balance: float):
+        self.__name = name
+        self.__acc_num = acc_num
+        self.__balance = balance
+
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+            self.__service_charge()
+        else:
+            raise ValueError("Deposit needs be bigger than zero!!!")
+
+    def withdraw(self, amount: float):
+        if amount < self.__balance:
+            self.__balance -= amount
+            self.__service_charge()
+        else:
+            raise ValueError("You don't have that much!!!")
+
+    def __service_charge(self):
+        self.__balance -= self.__balance * 0.01
+
+    @property
+    def balance(self):
+        return self.__balance
 
 
+if __name__ == '__main__':
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)
