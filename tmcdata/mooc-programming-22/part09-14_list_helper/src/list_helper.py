30fb6c31ac70248a21d5bb439e45a2e8a3bbf2b8
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-15 20:55:13 +0100

    Complete exercise: list helper

--- tmcdata/mooc-programming-22/part09-14_list_helper/src/list_helper.py
+++ tmcdata/mooc-programming-22/part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,34 @@
-# WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    def __init__(self, numbers: list):
+        self.__numbers = numbers
+
+    @classmethod
+    def greatest_frequency(self, my_list: list):
+        winner = ''
+        highest_count = 0
+        counter = 0
+        for num in my_list:
+            for num1 in my_list:
+                if num == num1:
+                    counter += 1
+            if counter > highest_count:
+                winner = num
+                highest_count = counter
+                counter = 0
+        return winner
+
+    @classmethod
+    def doubles(self, my_list: list):
+        numbers_copy = my_list[:]
+        unique_nums = []
+        for i in range(len(my_list)):
+            num = numbers_copy.pop(0)
+            if num not in unique_nums and num in numbers_copy:
+                unique_nums.append(num)
+        return len(unique_nums)
+
+
+if __name__ == '__main__':
+    numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+    print(ListHelper.greatest_frequency(numbers))
+    print(ListHelper.doubles(numbers))
