6c9d63f5da1eed3436f030a1fc12521a9202a6b3
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-19 15:40:24 +0100

    Download exercises: part 10

---
+++ tmcdata/mooc-programming-22/part10-01_laptop_computer/src/laptop_computer.py
@@ -0,0 +1,12 @@
+class Computer:
+    def __init__(self, model: str, speed: int):
+        self.__model = model
+        self.__speed = speed
+
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def speed(self):
+        return self.__speed
