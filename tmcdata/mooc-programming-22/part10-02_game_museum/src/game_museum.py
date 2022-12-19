2f13c9d5075293aa17b95591faec2fea24977895
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-19 16:48:30 +0100

    Complete exercise: game museum

--- tmcdata/mooc-programming-22/part10-02_game_museum/src/game_museum.py
+++ tmcdata/mooc-programming-22/part10-02_game_museum/src/game_museum.py
@@ -5,6 +5,7 @@ class ComputerGame:
         self.publisher = publisher
         self.year = year
 
+
 class GameWarehouse:
     def __init__(self):
         self.__games = []
@@ -15,3 +16,24 @@ class GameWarehouse:
     def list_games(self):
         return self.__games
 
+
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__()
+
+    def list_games(self):
+        games = super().list_games()
+        results = []
+        for game in games:
+            if game.year < 1990:
+                results.append(game)
+        return results
+
+
+if __name__ == '__main__':
+    museum = GameMuseum()
+    museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+    museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
+    museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+    for game in museum.list_games():
+        print(game.name)
