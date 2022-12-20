6a9ecd60986aaf8a6b0b778b3b184a8d4a051810
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-20 17:18:40 +0100

    Complete part 1:  simple date

--- tmcdata/mooc-programming-22/part10-08_simple_date/src/simple_date.py
+++ tmcdata/mooc-programming-22/part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,37 @@
-# WRITE YOUR SOLUTION HERE:
+class SimpleDate:
+    def __init__(self, day: int, month: int, year: int):
+        self.__day = day
+        self.__month = month
+        self.__year = year
 
+    def get_date(self):
+        return int(f"{self.__year}{self.__month:02d}{self.__day:02d}")
+
+    def __lt__(self, another):
+        return self.get_date() < another.get_date()
+
+    def __gt__(self, another):
+        return self.get_date() > another.get_date()
+
+    def __eq__(self, another):
+        return self.get_date() == another.get_date()
+
+    def __ne__(self, another):
+        return self.get_date() != another.get_date()
+
+    def __str__(self):
+        return f"{self.__day}.{self.__month}.{self.__year}"
+
+
+if __name__ == '__main__':
+    d1 = SimpleDate(4, 10, 2020)
+    d2 = SimpleDate(28, 12, 1985)
+    d3 = SimpleDate(28, 12, 1985)
+
+    print(d1)
+    print(d2)
+    print(d1 == d2)
+    print(d1 != d2)
+    print(d1 == d3)
+    print(d1 < d2)
+    print(d1 > d2)
