71170c65e885fd2eb7106bac6e386fc5394bacc4
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-20 16:03:38 +0100

    Complete part 4: money

--- tmcdata/mooc-programming-22/part10-07_money/src/money.py
+++ tmcdata/mooc-programming-22/part10-07_money/src/money.py
@@ -1,30 +1,40 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self.__euros = euros
+        self.__cents = cents
 
-    def amount(self, money):
-        return f"{money.euros}.{money.cents:02d}"
+    def __amount(self, money):
+        return float(f"{money.__euros}.{money.__cents:02d}")
 
     def __eq__(self, another):
-        amount1, amount2 = self.amount(self), self.amount(another)
+        amount1, amount2 = self.__amount(self), self.__amount(another)
         return amount1 == amount2
 
     def __lt__(self, another):
-        amount1, amount2 = self.amount(self), self.amount(another)
+        amount1, amount2 = self.__amount(self), self.__amount(another)
         return amount1 < amount2
 
     def __gt__(self, another):
-        amount1, amount2 = self.amount(self), self.amount(another)
+        amount1, amount2 = self.__amount(self), self.__amount(another)
         return amount1 > amount2
 
     def __ne__(self, another):
-        amount1, amount2 = self.amount(self), self.amount(another)
+        amount1, amount2 = self.__amount(self), self.__amount(another)
         return amount1 != amount2
 
+    def __add__(self, another):
+        amount1, amount2 = self.__amount(self), self.__amount(another)
+        return f"{(amount1 + amount2):.2f} eur"
+
+    def __sub__(self, another):
+        amount1, amount2 = self.__amount(self), self.__amount(another)
+        if amount1 - amount2 < 0:
+            raise ValueError('A negative result is not allowed')
+        return f"{(amount1 - amount2):.2f} eur"
+
     def __str__(self):
-        return f"{self.euros}.{self.cents:02d} eur"
+        return f"{self.__euros}.{self.__cents:02d} eur"
 
 
 if __name__ == '__main__':
@@ -47,3 +57,15 @@ if __name__ == '__main__':
     print(e1 != e2)
     print(e1 < e2)
     print(e1 > e2)
+
+    print('\nPart 4')
+    e1 = Money(4, 5)
+    e2 = Money(2, 95)
+
+    e3 = e1 + e2
+    e4 = e1 - e2
+
+    print(e3)
+    print(e4)
+
+    e5 = e2-e1
