bec84d3e56b4a641ea39ef459b4370e1b360137d
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-20 15:46:52 +0100

    Complete part 3: money

--- tmcdata/mooc-programming-22/part10-07_money/src/money.py
+++ tmcdata/mooc-programming-22/part10-07_money/src/money.py
@@ -4,10 +4,24 @@ class Money:
         self.euros = euros
         self.cents = cents
 
+    def amount(self, money):
+        return f"{money.euros}.{money.cents:02d}"
+
     def __eq__(self, another):
-        amount1 = f"{self.euros}.{self.cents:02d}"
-        amount2 = f"{another.euros}.{another.cents:02d}"
+        amount1, amount2 = self.amount(self), self.amount(another)
         return amount1 == amount2
+
+    def __lt__(self, another):
+        amount1, amount2 = self.amount(self), self.amount(another)
+        return amount1 < amount2
+
+    def __gt__(self, another):
+        amount1, amount2 = self.amount(self), self.amount(another)
+        return amount1 > amount2
+
+    def __ne__(self, another):
+        amount1, amount2 = self.amount(self), self.amount(another)
+        return amount1 != amount2
 
     def __str__(self):
         return f"{self.euros}.{self.cents:02d} eur"
@@ -23,9 +37,13 @@ if __name__ == '__main__':
 
     print('\nPart 2')
     e3 = Money(4, 10)
-
     print(e1)
     print(e2)
     print(e3)
     print(e1 == e2)
     print(e1 == e3)
+
+    print('\nPart 3')
+    print(e1 != e2)
+    print(e1 < e2)
+    print(e1 > e2)
