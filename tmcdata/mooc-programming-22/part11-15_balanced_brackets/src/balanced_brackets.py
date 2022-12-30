d106406e4d98275326e8b5a970d2bae91d15fa9a
djdevlam-apple <embryonic.dev@gmail.com>
2022-12-30 16:28:29 +0100

    Complete exercise: balanced brackets

--- tmcdata/mooc-programming-22/part11-15_balanced_brackets/src/balanced_brackets.py
+++ tmcdata/mooc-programming-22/part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,9 +1,45 @@
 
 def balanced_brackets(my_string: str):
+    brackets = '([])'
+
     if len(my_string) == 0:
         return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
-        return False
+    # remove first char if it's not a bracket
+    if my_string[0] not in brackets:
+        return balanced_brackets(my_string[1:])
+    # remove last char if it's not a bracket
+    if my_string[-1] not in brackets:
+        return balanced_brackets(my_string[0:-1])
 
-    # remove first and last character
-    return balanced_brackets(my_string[1:-1])
+    if ((my_string[0] == '(' and my_string[-1] == ')') or
+            (my_string[0] == '[' and my_string[-1] == ']')):
+        return balanced_brackets(my_string[1:-1])
+
+    return False
+
+
+if __name__ == "__main__":
+    print('\nBase function')
+    ok = balanced_brackets("(((())))")
+    print(ok)
+    # there is one closing bracket too many, so this produces False
+    ok = balanced_brackets("()())")
+    print(ok)
+    # this one starts with a closing bracket, False again
+    ok = balanced_brackets(")()")
+    print(ok)
+    # this produces False because the function only handles entirely nested brackets
+    ok = balanced_brackets("()(())")
+    print(ok)
+
+    print('\nMy solution')
+    ok = balanced_brackets("([([])])")
+    print(ok)
+    ok = balanced_brackets("(python version [3.7]) please use this one!")
+    print(ok)
+    # this is no good, the closing bracket doesn't match
+    ok = balanced_brackets("(()]")
+    print(ok)
+    # different types of brackets are mismatched
+    ok = balanced_brackets("([bad egg)]")
+    print(ok)
