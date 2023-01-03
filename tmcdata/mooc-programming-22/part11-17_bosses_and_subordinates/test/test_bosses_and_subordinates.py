598eae1918a10a85ca3a6ffe69b600cdd63e6bb7
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-03 18:00:28 +0100

    Test & submit

--- tmcdata/mooc-programming-22/part11-17_bosses_and_subordinates/test/test_bosses_and_subordinates.py
+++ tmcdata/mooc-programming-22/part11-17_bosses_and_subordinates/test/test_bosses_and_subordinates.py
@@ -7,16 +7,18 @@ from functools import reduce
 import os
 import os.path
 import textwrap
-import inspect, re
+import inspect
+import re
 
 exercise = 'src.bosses_and_subordinates'
+
 
 @points('11.bosses_and_subordinates')
 class BossesAndSubordinatesTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         with patch('builtins.input', side_effect=[AssertionError("Asking input from the user was not expected.")]):
-           cls.module = load_module(exercise, 'en')
+            cls.module = load_module(exercise, 'en')
 
     def test_0a_main_program_ok(self):
         ok, line = check_source(self.module)
@@ -30,19 +32,20 @@ block. The following row should be moved:
         try:
             from src.bosses_and_subordinates import count_subordinates
         except Exception as e:
-            self.fail(f'Your program should contain function named count_subordinates.')
+            self.fail(
+                f'Your program should contain function named count_subordinates.')
 
     def test_2_type_of_return_value(self):
         try:
             from src.bosses_and_subordinates import count_subordinates, Employee
             val = count_subordinates(Employee("Joe"))
         except Exception as e:
-            self.assertTrue(False, f"Function threw an error when it was called as follows:" + 
-                f'\ncount_subordinates(Employee(\"Joe\"))\n{e}')
-        taip = str(type(val)).replace("<class '","").replace("'>","")
-        self.assertTrue(type(val) == int, f"Function count_subordinates should return a value, which is of type int," +  
-            f" now it returns a value {val} which is of type {taip}\n when it is called as follows:\n" +  
-            'count_subordinates(Employee(\"Joe\"))')
+            self.assertTrue(False, f"Function threw an error when it was called as follows:" +
+                            f'\ncount_subordinates(Employee(\"Joe\"))\n{e}')
+        taip = str(type(val)).replace("<class '", "").replace("'>", "")
+        self.assertTrue(type(val) == int, f"Function count_subordinates should return a value, which is of type int," +
+                        f" now it returns a value {val} which is of type {taip}\n when it is called as follows:\n" +
+                        'count_subordinates(Employee(\"Joe\"))')
 
     def test_3_function(self):
         from src.bosses_and_subordinates import count_subordinates, Employee
@@ -71,11 +74,13 @@ block. The following row should be moved:
         t2.add_subordinate(t11)
         t5.add_subordinate(t6)
 
-        tests = [(t1,11),(t2,3),(t3,8),(t4,0),(t5,1),(t6,0),(t7,0),(t8,0),(t9,4),(t10,0),(t11,0),(t12,0)]
+        tests = [(t1, 11), (t2, 3), (t3, 8), (t4, 0), (t5, 1), (t6, 0),
+                 (t7, 0), (t8, 0), (t9, 4), (t10, 0), (t11, 0), (t12, 0)]
         for test in tests:
             result = count_subordinates(test[0])
-            self.assertEqual(result, test[1], f"Employee {test[0].name} should have {test[1]} subordinates, but function returns {result} subordinates "+
-                                               "(you can look the staff structure from the tests")
+            self.assertEqual(result, test[1], f"Employee {test[0].name} should have {test[1]} subordinates, but function returns {result} subordinates " +
+                             "(you can look the staff structure from the tests")
+
 
 if __name__ == '__main__':
     unittest.main()
