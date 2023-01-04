a5651b4ddb85385da6526efd4c6f2aa25e353939
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-04 13:58:03 +0100

    Complete exercise: order book - part 3: method - mark finished

--- tmcdata/mooc-programming-22/part11-18_order_book/src/order_book.py
+++ tmcdata/mooc-programming-22/part11-18_order_book/src/order_book.py
@@ -1,5 +1,4 @@
 class Task:
-    complete = False
     task_num = 0
 
     def __init__(self, description: str, programmer: str, workload: int):
@@ -8,16 +7,18 @@ class Task:
         self.workload = workload
         Task.task_num += 1
         self.id = Task.task_num
+        self.complete = False
 
     def mark_finished(self):
-        Task.complete = True
+        print('marked finished in class Task')
+        self.complete = True
 
     def is_finished(self):
-        return Task.complete
+        return self.complete
 
     def __str__(self):
         shared_text = f"{self.id}: {self.description} ({self.workload} hours), programmer {self.programmer}"
-        ending = "FINISHED" if Task.complete else 'NOT FINISHED'
+        ending = "FINISHED" if self.complete else 'NOT FINISHED'
         return f"{shared_text} {ending}"
 
 
@@ -40,22 +41,38 @@ class OrderBook:
     def programmers(self):
         return self.get_names()
 
+    def mark_finished(self, id: int):
+        for task in self.orders:
+            if task.id == id:
+                task.mark_finished()
+
 
 if __name__ == '__main__':
-    print('\nPart 1:')
-    t1 = Task("program hello world", "Eric", 3)
-    print(t1.id, t1.description, t1.programmer, t1.workload)
-    print(t1)
-    print(t1.is_finished())
-    t1.mark_finished()
-    print(t1)
-    print(t1.is_finished())
-    t2 = Task("program webstore", "Adele", 10)
-    t3 = Task("program mobile app for workload accounting", "Eric", 25)
-    print(t2)
-    print(t3)
+    # print('\nPart 1:')
+    # t1 = Task("program hello world", "Eric", 3)
+    # print(t1.id, t1.description, t1.programmer, t1.workload)
+    # print(t1)
+    # print(t1.is_finished())
+    # t1.mark_finished()
+    # print(t1)
+    # print(t1.is_finished())
+    # t2 = Task("program webstore", "Adele", 10)
+    # t3 = Task("program mobile app for workload accounting", "Eric", 25)
+    # print(t2)
+    # print(t3)
 
-    print('\nPart 2:')
+    # print('\nPart 2:')
+    # orders = OrderBook()
+    # orders.add_order("program webstore", "Adele", 10)
+    # orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    # orders.add_order("program app for practising mathematics", "Adele", 100)
+    # for order in orders.all_orders():
+    #     print(order)
+    # print()
+    # for programmer in orders.programmers():
+    #     print(programmer)
+
+    print('\nPart 3:')
     orders = OrderBook()
     orders.add_order("program webstore", "Adele", 10)
     orders.add_order("program mobile app for workload accounting", "Eric", 25)
@@ -64,7 +81,8 @@ if __name__ == '__main__':
     for order in orders.all_orders():
         print(order)
 
-    print()
+    orders.mark_finished(1)
+    orders.mark_finished(2)
 
-    for programmer in orders.programmers():
-        print(programmer)
+    for order in orders.all_orders():
+        print(order)
