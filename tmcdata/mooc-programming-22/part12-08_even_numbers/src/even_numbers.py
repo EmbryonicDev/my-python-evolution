2a0f9991a0ca9c221e3ebb60f9df24e576d9da01
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-05 20:41:46 +0100

    Complete exercise: even numbers

--- tmcdata/mooc-programming-22/part12-08_even_numbers/src/even_numbers.py
+++ tmcdata/mooc-programming-22/part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,13 @@
-# Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    if beginning % 2 != 0:
+        beginning += 1
+    while beginning <= maximum:
+        if beginning % 2 == 0:
+            yield beginning
+            beginning += 2
+
+
+if __name__ == '__main__':
+    numbers = even_numbers(16, 33)
+    for number in numbers:
+        print(number)
