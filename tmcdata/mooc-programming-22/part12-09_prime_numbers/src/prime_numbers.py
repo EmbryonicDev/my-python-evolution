d9c1277e9c41b84e5433e605563ca30d194db03a
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-06 17:01:28 +0100

    Complete exercise: prime numbers

--- tmcdata/mooc-programming-22/part12-09_prime_numbers/src/prime_numbers.py
+++ tmcdata/mooc-programming-22/part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,20 @@
-# Write your solution here
+def prime_numbers():
+    def isprime(n):
+        for x in range(2, n):
+            if n % x == 0:
+                return False
+        return True
+
+    # generate the first 2000 prime numbers
+    for i in range(2, 2001):
+        if isprime(i):
+            yield i
+
+
+if __name__ == '__main__':
+    numbers = prime_numbers()
+    for i in range(30):
+        print(next(numbers))
+
+    # temp = [x for x in range(1, 100) if prime_numbers()]
+    # print(temp)
