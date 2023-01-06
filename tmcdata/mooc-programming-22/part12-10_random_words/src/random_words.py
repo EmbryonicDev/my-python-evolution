41bfd48dcac47cbae0b0128accbfdb4d41e4bc7f
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-06 19:25:52 +0100

    Complete exercise: random words

--- tmcdata/mooc-programming-22/part12-10_random_words/src/random_words.py
+++ tmcdata/mooc-programming-22/part12-10_random_words/src/random_words.py
@@ -1,1 +1,20 @@
-# Write your solution here:
+from random import choice
+
+
+def word_generator(characters: str, length: int, amount: int):
+    # first attempt
+    # def get_char(length: int):
+    #     word = ''
+    #     for i in range(length):
+    #         word += characters[random.randint(0, len(characters)-1)]
+    #     return word
+    # return (get_char(length) for i in range(amount))
+
+    # second attempt
+    return ("".join([choice(characters) for i in range(length)]) for j in range(amount))
+
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
