9e335f13152086c03c54c6995e158800990ee511
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-09 18:45:10 +0100

    credits: complete part 1

--- tmcdata/mooc-programming-22/part12-13_credits/src/credits.py
+++ tmcdata/mooc-programming-22/part12-13_credits/src/credits.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+
 class CourseAttempt:
     def __init__(self, course_name: str, grade: int, credits: int):
         self.course_name = course_name
@@ -9,4 +10,14 @@ class CourseAttempt:
     def __str__(self):
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
-# Write your solution
+
+def sum_of_all_credits(attempts: list):
+    return reduce(lambda acc, curr: acc + curr.credits, attempts, 0)
+
+
+if __name__ == '__main__':
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
