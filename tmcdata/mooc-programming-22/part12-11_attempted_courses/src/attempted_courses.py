9e4adeafee2f198c944db8090e537b7aba3245bc
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-09 16:57:12 +0100

    Complete exercise: attempted courses - Part 1

--- tmcdata/mooc-programming-22/part12-11_attempted_courses/src/attempted_courses.py
+++ tmcdata/mooc-programming-22/part12-11_attempted_courses/src/attempted_courses.py
@@ -7,4 +7,15 @@ class CourseAttempt:
     def __str__(self):
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
 
-# Write your solution here
+
+def names_of_students(attempts: list):
+    return list(map(lambda s: s.student_name, attempts))
+
+
+if __name__ == '__main__':
+    print('\nPart 1 -')
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+    for name in names_of_students([s1, s2, s3]):
+        print(name)
