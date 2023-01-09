d1171d66c036ba4e68acb0885d31747a90cb2b03
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-09 17:26:24 +0100

    filtering attempts: complete part 1

--- tmcdata/mooc-programming-22/part12-12_filtering_attempts/src/filtering_attempts.py
+++ tmcdata/mooc-programming-22/part12-12_filtering_attempts/src/filtering_attempts.py
@@ -8,3 +8,18 @@ class CourseAttempt:
         return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
 
 
+def accepted(attempts: list):
+    def at_least_one(attempt):
+        if attempt.grade > 1:
+            return attempt.grade
+
+    return list(filter(at_least_one, attempts))
+
+
+if __name__ == '__main__':
+    print('\nPart 1 -')
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 0)
+    for attempt in accepted([s1, s2, s3]):
+        print(attempt)
