d723daefc9909eea7531719b210fa8d27718fcf0
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-09 20:52:56 +0100

    regular expressions: improve part 1: ternary operator

--- tmcdata/mooc-programming-22/part12-14_regular_expressions/src/regular_expressions.py
+++ tmcdata/mooc-programming-22/part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,13 @@
 # Write your solution here
 import re
+
+
+def is_dotw(my_string: str):
+    return True if re.search('Mon|Tue|Wed|Thu|Fri|Sat|Sun', my_string) else False
+
+
+if __name__ == "__main__":
+    print('\nPart 1:')
+    print(is_dotw("Mon"))
+    print(is_dotw("Fri"))
+    print(is_dotw("Tui"))
