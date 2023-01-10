00c5f9b544ac915938ff01fda93509a7a6c22295
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-10 13:06:43 +0100

    regular expressions: complete part 2

--- tmcdata/mooc-programming-22/part12-14_regular_expressions/src/regular_expressions.py
+++ tmcdata/mooc-programming-22/part12-14_regular_expressions/src/regular_expressions.py
@@ -6,8 +6,16 @@ def is_dotw(my_string: str):
     return True if re.search('Mon|Tue|Wed|Thu|Fri|Sat|Sun', my_string) else False
 
 
+def all_vowels(my_string: str):
+    return True if re.search('^[aeiou]*$', my_string) else False
+
+
 if __name__ == "__main__":
     print('\nPart 1:')
     print(is_dotw("Mon"))
     print(is_dotw("Fri"))
     print(is_dotw("Tui"))
+
+    print('\nPart 2:')
+    print(all_vowels("eioueioieoieou"))
+    print(all_vowels("autoooo"))
