0b8978394346068a515b65e6518d77e702b4246f
djdevlam-apple <embryonic.dev@gmail.com>
2023-01-10 13:26:18 +0100

    regular expressions: complete part 3

--- tmcdata/mooc-programming-22/part12-14_regular_expressions/src/regular_expressions.py
+++ tmcdata/mooc-programming-22/part12-14_regular_expressions/src/regular_expressions.py
@@ -10,6 +10,10 @@ def all_vowels(my_string: str):
     return True if re.search('^[aeiou]*$', my_string) else False
 
 
+def time_of_day(my_string: str):
+    return True if re.search('^([0-1]?[0-9]|2[0-3])(:[0-5][0-9]){2}', my_string) else False
+
+
 if __name__ == "__main__":
     print('\nPart 1:')
     print(is_dotw("Mon"))
@@ -19,3 +23,9 @@ if __name__ == "__main__":
     print('\nPart 2:')
     print(all_vowels("eioueioieoieou"))
     print(all_vowels("autoooo"))
+
+    print('\nPart 3:')
+    print(time_of_day("12:43:01"))
+    print(time_of_day("AB:01:CD"))
+    print(time_of_day("17:59:59"))
+    print(time_of_day("33:66:77"))
