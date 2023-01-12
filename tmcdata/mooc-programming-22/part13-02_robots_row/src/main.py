08dbe46e5abda8afce55ae23fcdd9f9b5decf16a
djdevlam <embryonic.dev@gmail.com>
2023-01-12 19:48:21 +0100

    Complete exercise: robots row

--- tmcdata/mooc-programming-22/part13-02_robots_row/src/main.py
+++ tmcdata/mooc-programming-22/part13-02_robots_row/src/main.py
@@ -1,1 +1,18 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0, 0, 0))
+for i in range(10):
+    window.blit(robot, (50+50*i, 100))
+    height += width
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
