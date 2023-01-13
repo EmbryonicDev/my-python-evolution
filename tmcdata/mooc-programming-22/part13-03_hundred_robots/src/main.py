2ca9580db8d8d5a7c29652cd2156150797e82a03
djdevlam <embryonic.dev@gmail.com>
2023-01-13 13:41:05 +0100

    Complete exercise: hundred robots

--- tmcdata/mooc-programming-22/part13-03_hundred_robots/src/main.py
+++ tmcdata/mooc-programming-22/part13-03_hundred_robots/src/main.py
@@ -1,1 +1,23 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load('robot.png')
+
+screen.fill((0, 0, 0))
+
+y = 100
+x = robot.get_width()
+for i in range(10):
+    for j in range(10):
+        screen.blit(robot, (x+40*j, y))
+    x += 10
+    y += 20
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
