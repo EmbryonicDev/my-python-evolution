533d47d6d0639240261348e4ddbe6bf1c4fc8d1e
djdevlam <embryonic.dev@gmail.com>
2023-01-18 20:49:01 +0100

    Complete exercise: robot & mouse

--- tmcdata/mooc-programming-22/part13-14_robot_and_mouse/src/main.py
+++ tmcdata/mooc-programming-22/part13-14_robot_and_mouse/src/main.py
@@ -1,1 +1,19 @@
-# WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION:
+            robot_x = event.pos[0]-robot.get_width()/2
+            robot_y = event.pos[1]-robot.get_height()/2
+
+            window.fill((0, 0, 0))
+            window.blit(robot, (robot_x, robot_y))
+            pygame.display.flip()
+
+        if event.type == pygame.QUIT:
+            exit(0)
