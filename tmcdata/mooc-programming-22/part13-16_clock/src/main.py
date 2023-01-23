e75d4a88bf5453632c6137c26ca131379f102924
djdevlam <embryonic.dev@gmail.com>
2023-01-23 15:29:38 +0100

    Clock: Randomize all colors on each tick

--- tmcdata/mooc-programming-22/part13-16_clock/src/main.py
+++ tmcdata/mooc-programming-22/part13-16_clock/src/main.py
@@ -1,8 +1,9 @@
 import pygame
-from datetime import datetime
 import math
+import random
+from datetime import datetime
 
-width, height = 640, 480
+width, height = 2000, 2000
 center = width/2, height/2
 radius = center[1]
 radius_list = {'sec': radius*0.8, 'min': radius *
@@ -11,7 +12,6 @@ radius_list = {'sec': radius*0.8, 'min': radius *
 pygame.init()
 window = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-
 # for hours, minutes and seconds
 clock60 = dict(zip(range(60), range(0, 360, 6)))
 
@@ -24,32 +24,37 @@ def get_clock_pos(clock_dict, clock_hand, key):
     return x, y
 
 
+def get_random_coords():
+    return (random.randint(0, 255), random.randint(
+        0, 255), random.randint(0, 255))
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("You've quit the game...")
             exit()
 
-    window.fill((0)*3)
+    window.fill(get_random_coords())
 
-    # solid red circle
-    pygame.draw.circle(window, (255, 0, 0), (center), radius*0.95)
-    # solid black circle, slightly smaller than red circle
-    pygame.draw.circle(window, (0, 0, 0), (center), radius*0.925)
-    # Small red circle in center
-    pygame.draw.circle(window, (255, 0, 0), center, radius*0.05)
+    # Main circle
+    pygame.draw.circle(window, (get_random_coords()), (center), radius*0.95)
+    # Smaller circle for watch face
+    pygame.draw.circle(window, (get_random_coords()), (center), radius*0.925)
+    # Small circle in middle
+    pygame.draw.circle(window, (get_random_coords()), center, radius*0.05)
 
     now = datetime.now()
     hour, minute, second = ((now.hour % 12) * 5 +
                             now.minute // 12) % 60, now.minute, now.second
     # seconds hand
-    pygame.draw.line(window, (0, 0, 255), center,
+    pygame.draw.line(window, (get_random_coords()), center,
                      get_clock_pos(clock60, second, 'sec'), 3)
     # minutes hand
-    pygame.draw.line(window, (0, 0, 255), center,
+    pygame.draw.line(window, (get_random_coords()), center,
                      get_clock_pos(clock60, minute, 'min'), 4)
     # hour hand
-    pygame.draw.line(window, (0, 0, 255), center,
+    pygame.draw.line(window, (get_random_coords()), center,
                      get_clock_pos(clock60, hour, 'hour'), 6)
 
     pygame.display.flip()
