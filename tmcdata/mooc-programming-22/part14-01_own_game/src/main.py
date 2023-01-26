781511ca0d97667a6bafc5f2c52fd116ad6fa901
djdevlam <embryonic.dev@gmail.com>
2023-01-26 13:34:31 +0100

    Create MovingObject Class, def release_coins, def move_coin

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -1,4 +1,13 @@
 import pygame
+import random
+
+
+class MovingObject:
+    def __init__(self, x, y, directions):
+        self.x = x
+        self.y = y
+        self.x_speed = directions[0]
+        self.y_speed = directions[1]
 
 
 class GetCoin:
@@ -12,12 +21,14 @@ class GetCoin:
         self.window = pygame.display.set_mode(
             (self.width, self.height+self.info_board))
         pygame.display.set_caption('Coin Chaser')
+
         self.to_left, self.to_right, self.to_up, self.to_down = False, False, False, False
         self.clock = pygame.time.Clock()
 
         self.load_images()
         self.robot_height = self.images['robot'].get_height()
         self.robot_width = self.images['robot'].get_width()
+        self.coin_width = self.images['coin'].get_width()
         self.new_game()
         self.main_loop()
 
@@ -25,6 +36,7 @@ class GetCoin:
         while True:
             self.check_events()
             self.draw_window()
+            self.move_coin()
 
     def check_events(self):
         for event in pygame.event.get():
@@ -51,11 +63,16 @@ class GetCoin:
 
             if event.type == pygame.QUIT:
                 exit()
+
         self.move_bot()
 
     def draw_window(self):
         self.window.fill((0)*3)
         self.window.blit(self.images['robot'], self.robot_pos)
+
+        for coin in self.coins:
+            self.window.blit(self.images['coin'],
+                             (coin.x, coin.y))
 
         pygame.display.flip()
         self.clock.tick(60)
@@ -70,13 +87,36 @@ class GetCoin:
         if self.to_up and self.robot_pos[1] >= 0:
             self.robot_pos[1] -= 2
 
+    def move_coin(self):
+        for coin in self.coins:
+            if coin.x <= 0 or coin.x + self.coin_width >= self.width:
+                coin.x_speed *= -1
+            if coin.y <= 0 or coin.y + self.coin_width >= self.height:
+                coin.y_speed *= -1
+
+            coin.x += coin.x_speed
+            coin.y += coin.y_speed
+
+    def get_direction(self):
+        return [random.choice(
+            [-8, 8]), random.choice((-8, 8))]
+
     def load_images(self):
         self.images = {}
         for name in ['coin', 'robot', 'door', 'monster']:
             self.images[name] = pygame.image.load(name+'.png')
 
+    def release_coins(self):
+        self.coins = []
+        for i in range(5):
+            new_coin = MovingObject(random.randint(
+                0, self.width - self.coin_width), random.randint(
+                0, self.height - self.coin_width), self.get_direction())
+            self.coins.append(new_coin)
+
     def new_game(self):
         self.robot_pos = [0, self.height - self.robot_height]
+        self.release_coins()
 
 
 if __name__ == '__main__':
