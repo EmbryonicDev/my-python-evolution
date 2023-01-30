a8ca543d43a058429c35755c760e0308bd6cd635
djdevlam <embryonic.dev@gmail.com>
2023-01-30 19:17:13 +0100

    Remove redundant variable: monsters from GetCoin root

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -72,7 +72,6 @@ class GetCoin:
 
         self.game_font = pygame.font.SysFont('Arial', 36)
         self.door = Door([self.width, self.height])
-        self.monsters = []
         self.clock = pygame.time.Clock()
         self.new_game()
         self.main_loop()
