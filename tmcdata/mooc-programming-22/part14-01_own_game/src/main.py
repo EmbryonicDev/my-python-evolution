b4b227015ad97637415ec1449e2689e1624e71f7
djdevlam <embryonic.dev@gmail.com>
2023-01-31 14:08:52 +0100

    Modify release_monsters to check robot position before releasing monsters

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -267,7 +267,10 @@ class GetCoin:
         for i in range(self.level):
             monster = MovingObject([self.width, self.height], 'monster')
             monster.x = random.randint(0, self.width-monster.width)
-            monster.y = random.randint(0, self.height*0.2)
+            # release monsters on opposite side of robot's death
+            monster.y = (random.randint(0, self.height*0.2)
+                         if self.bot.y > self.height / 2
+                         else random.randint(self.height*0.8, self.height-monster.height))
             self.monsters.append(monster)
 
     def new_game(self):
