d1c0386804d859c62b73ddd69c85e3302ccef270
djdevlam <embryonic.dev@gmail.com>
2023-02-01 17:01:30 +0100

    Cosmetics

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -64,7 +64,8 @@ class MovingMonster(MovingObject):
         self.x = random.randint(0, self.screen_width-self.width)
         self.y = (random.randint(0, self.screen_height*0.2)
                   if bot_y > self.screen_height / 2
-                  else random.randint(self.height*0.8, self.screen_height-self.height))
+                  else random.randint(self.height*0.8,
+                                      self.screen_height-self.height))
 
 
 class MovingCoin(MovingObject):
