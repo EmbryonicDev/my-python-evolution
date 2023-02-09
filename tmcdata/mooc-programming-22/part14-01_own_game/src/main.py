56499fafc63481150655c4e0023e74b575689c8f
djdevlam <embryonic.dev@gmail.com>
2023-02-09 20:24:18 +0100

    Only allow game pause if not game_over

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -191,7 +191,8 @@ class GetCoin:
                 if event.key == pygame.K_F2:
                     self.new_game()
                 if event.key == pygame.K_SPACE:
-                    self.game_paused = True if self.game_paused == False else False
+                    if not self.game_over:
+                        self.game_paused = True if self.game_paused == False else False
                 if event.key == pygame.K_ESCAPE:
                     exit()
 
