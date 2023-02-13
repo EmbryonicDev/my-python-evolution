5613a58c04f2b6cbf3fdb01bc4757d6f6e7bfeae
djdevlam <embryonic.dev@gmail.com>
2023-02-13 22:06:37 +0100

    Implement blit_text_bg() & blit_text()

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -412,11 +412,10 @@ class GetCoin:
         # if bonus ball is on screen, prompt user to catch it
         if self.bonus_coin.x > -1:
             # rectangle behind bonus text
-            pygame.draw.rect(self.window, (0, 0, 0), (self.width/2-game_text.get_width()/2, self.total_height -
-                                                      self.bonus_board/2 - game_text.get_height()/2, game_text.get_width(), game_text.get_height()))
+            blit_text_bg()
             # game text to window
-            self.window.blit(game_text, (self.width*.5-(game_text.get_width()/2),
-                                         self.height + self.info_board + (self.info_board*0.35)))
+            blit_text()
+
 
     def release_coins(self):
         print('coins released')
