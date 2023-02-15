7d136ab0c54e0499946118e5bcfcccbe565e6f6c
djdevlam <embryonic.dev@gmail.com>
2023-02-15 17:24:38 +0100

    Add cupcake power to BonusCoin.dict

--- tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/main.py
@@ -76,7 +76,12 @@ class BonusCoin(MovingCoin):
         # ['eat', 'kill', 'freeze', 'add health', 'multiply', 'invincible']
 
         self.dict = random.choice(
-            [{'power': 'freeze', 'user_prompt': 'Ghosts are Frozen'}, {'power': 'speed', 'user_prompt': 'Supercharged Ghosts! Be Careful'}])
+            [
+                # {'power': 'freeze', 'user_prompt': 'Ghosts are Frozen'},
+                # {'power': 'speed up', 'user_prompt': 'Super Fast Ghosts! Be Careful!'},
+                {'power': 'cupcake', 'user_prompt': 'Eat the Cupcakes! Bitch!'}
+
+            ])
         self.power = self.dict['power']
         self.user_prompt = self.dict['user_prompt']
         self.freeze()
