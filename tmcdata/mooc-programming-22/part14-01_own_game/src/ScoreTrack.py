7a30637851ff089140b6acd74c38dc2044830e31
EmbryonicDev <embryonic.dev@gmail.com>
2023-02-23 21:57:53 +0100

    work on Player / HighScores
    saving to work on other computer tomorrow

--- tmcdata/mooc-programming-22/part14-01_own_game/src/ScoreTrack.py
+++ tmcdata/mooc-programming-22/part14-01_own_game/src/ScoreTrack.py
@@ -2,35 +2,47 @@ class Player:
     def __init__(self, name: str):
         self.name = name
         self.points = 0
-        self.level = 0
-
-    def add_point(self):
-        self.points += 1
-
-    def add_point(self):
-        self.level += 1
-
+        self.level = 0  
+        
+    def update_player(self, points, level):
+        self.points, self.level = points, level
+        
 
 class HighScores:
     def __init__(self):
         self.top_ten_scores = []
-
+        self.player_count = len(self.top_ten_scores)
+        
     def update_scores(self, Player):
         if self.file_exists():
             self.text_to_list()
 
-        if len(self.top_ten_scores) == 10:
+        if self.player_count == 10:
             if Player.points > self.top_ten_scores[9].points:
                 self.top_ten_scores.pop(9)
+                
+        if self.player_count >= 2:
+            self.sort_scores()
 
         self.top_ten_scores.append(Player)
-        self.sort_scores()
         self.list_to_text()
         print(self.top_ten_scores)
+    
+    def text_to_list(self):
+        with open('scores.txt', 'r') as file:
+            for line in file:
+                if len(line) > 3:
+                    line = line.replace('\n', '')
+                    parts = line.split(';')
+                    player = Player(parts[0])
+                    print(parts)
+                    player.update_player(parts[1], parts[2])
+                    self.top_ten_scores.append(player)    
 
     def sort_scores(self):
-        self.top_ten_scores = sorted(
-            self.top_ten_scores, key=lambda x: x.points, reverse=True)
+        if self.player_count > 0:
+            self.top_ten_scores = sorted(
+                self.top_ten_scores, key=lambda x: x[points], reverse=True)
 
     def file_exists(self):
         file = 'scores.txt'
@@ -42,13 +54,6 @@ class HighScores:
             f = open(file, 'w+')
             print("File Created")
             return True
-
-    def text_to_list(self):
-        with open('scores.txt', 'r') as file:
-            for line in file:
-                parts = line.split(';')
-                player = Player(parts[0], parts[1], parts[3])
-                self.top_ten_scores.append(player)
 
     def list_to_text(self):
         with open('scores.txt', 'w') as file:
